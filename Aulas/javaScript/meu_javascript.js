98701c2732ef7ea21915692753f6e02dce041faa
vitorguimars <vitorguima.rs@bol.com.br>
2019-07-23 15:35:12 -0300

    Atualizado arquivos do curso inicio java script

---
+++ Aulas/javaScript/meu_javascript.js
@@ -0,0 +1,9 @@
+alert('Este é um teste de inclusão de js á partir de um arquivo externo');
+
+
+//Comentário de uma liha
+
+/* Comentário de multiplas linhas
+Este tipo de de comentário permite várias 
+linhas sejma comentadas */
+
